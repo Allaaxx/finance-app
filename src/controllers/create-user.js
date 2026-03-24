33c7630735f487193b4677e15dc8b3ee12b6b482
Allaaxx <allanrao358@gmail.com>
2026-03-24 11:39:09 -0300

    feat: add helpers funcitons to user controllers

--- src/controllers/create-user.js
+++ src/controllers/create-user.js
@@ -1,7 +1,12 @@
-import validator from 'validator';
 import { EmailAlreadyInUserError } from '../errors/user.js';
 import { CreateUserUseCase } from '../use-cases/create-user.js';
-import { badRequest, created, serverError } from './helpers.js';
+import { badRequest, created, serverError } from './helpers/http.js';
+import {
+    checkIfEmailIsValid,
+    checkIfPasswordIsValid,
+    EmailIsAlreadyInUseResponse,
+    invalidPasswordResponse,
+} from './helpers/user.js';
 
 export class CreateUserController {
     async execute(httpRequest) {
@@ -21,20 +26,16 @@ export class CreateUserController {
                 }
             }
 
-            const passwordIsNotValid = params.password.length < 6;
+            const passwordIsValid = checkIfPasswordIsValid(params.password);
 
-            if (passwordIsNotValid) {
-                return badRequest({
-                    message: 'Password must be at least 6 characters',
-                });
+            if (passwordIsValid) {
+                return invalidPasswordResponse();
             }
 
-            const emailIsValid = validator.isEmail(params.email);
+            const emailIsValid = checkIfEmailIsValid(params.email);
 
             if (!emailIsValid) {
-                return badRequest({
-                    message: 'Invalid e-mail. Please provide a valid one.',
-                });
+                EmailIsAlreadyInUseResponse();
             }
             const createUserUseCase = new CreateUserUseCase();
 
