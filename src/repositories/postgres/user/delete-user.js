32a22827e451821c01b9500b40ef273b134cb4c0
Allaaxx <allanrao358@gmail.com>
2026-04-03 12:17:34 -0300

    feat: ensure DeleteUser and GetUserByIdRepository use Prisma

--- src/repositories/postgres/user/delete-user.js
+++ src/repositories/postgres/user/delete-user.js
@@ -1,16 +1,15 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js';
-
+import { prisma } from '../../../../prisma/prisma.js';
 export class PostgresDeleteUserRepository {
     async execute(userId) {
-        const deletedUser = await PostgresHelper.query(
-            `
-            DELETE FROM users 
-            WHERE id = $1 
-            RETURNING *
-            `,
-            [userId],
-        );
-
-        return deletedUser[0];
+        try {
+            return await prisma.user.delete({
+                where: {
+                    id: userId,
+                },
+            });
+            // eslint-disable-next-line no-unused-vars
+        } catch (error) {
+            return null;
+        }
     }
 }
