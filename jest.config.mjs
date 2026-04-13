9e5a03cbd14b0d0973cdc9d8d58d3cbef8dc54cc
Allaaxx <allanrao358@gmail.com>
2026-04-13 11:33:25 -0300

    chore: update jest configuration and add @faker-js/faker dependency

--- jest.config.mjs
+++ jest.config.mjs
@@ -10,6 +10,10 @@ const config = {
     modulePathIgnorePatterns: ['<rootDir>/.postgres-data/'],
     testPathIgnorePatterns: ['<rootDir>/.postgres-data/'],
     watchPathIgnorePatterns: ['<rootDir>/.postgres-data/'],
+    transform: {
+        '^.+\\.[cm]?js$': 'babel-jest',
+    },
+    transformIgnorePatterns: ['/node_modules/(?!@faker-js/faker/)'],
 };
 
 export default config;
