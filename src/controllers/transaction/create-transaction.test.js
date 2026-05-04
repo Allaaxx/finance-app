f28654ebf3d1a4cf0662806e1d66b0c1c4b3177b
Allaaxx <allanrao358@gmail.com>
2026-05-04 08:23:24 -0300

    test: ensure CreateTransactionController calls CreateTransactionUseCase with correct values

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -161,4 +161,13 @@ describe('Create Transaction Controller', () => {
 
         expect(response.statusCode).toBe(500);
     });
+
+    it('should call CreateTransactionUseCase with correct values', async () => {
+        const { sut, createTransactionUseCase } = makeSut();
+        const executeSpy = jest.spyOn(createTransactionUseCase, 'execute');
+
+        await sut.execute(baseHttpRequest);
+
+        expect(executeSpy).toHaveBeenCalledWith(baseHttpRequest.body);
+    });
 });
