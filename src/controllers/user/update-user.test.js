6272213655d2e30e5426ee92185e324367a25b79
Allaaxx <allanrao358@gmail.com>
2026-05-04 11:16:02 -0300

    test: ensure UpdateUserController returns 404 if not find user

--- src/controllers/user/update-user.test.js
+++ src/controllers/user/update-user.test.js
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker';
-import { EmailAlreadyInUseError } from '../../errors/user';
+import { EmailAlreadyInUseError, UserNotFoundError } from '../../errors/user';
 import { UpdateUserController } from './update-user.js';
 
 describe('UpdateUserController', () => {
@@ -130,5 +130,16 @@ describe('UpdateUserController', () => {
             httpRequest.params.userId,
             httpRequest.body,
         );
+    });
+
+    it('should return 404 if UpdateUserUseCase throws with UserNotFoundError', async () => {
+        const { sut, updateUserUseCase } = makeSut();
+        jest.spyOn(updateUserUseCase, 'execute').mockRejectedValueOnce(
+            new UserNotFoundError(faker.string.uuid()),
+        );
+
+        const response = await sut.execute(httpRequest);
+
+        expect(response.statusCode).toBe(404);
     });
 });
