16f383d3f081a589b706ae38be041a653dabce53
Allaaxx <allanrao358@gmail.com>
2026-05-15 11:43:39 -0300

    test: ensure UpdateUserUseCase throws an error if UpdateUserRepository fails

--- src/use-cases/user/update-user.test.js
+++ src/use-cases/user/update-user.test.js
@@ -151,4 +151,20 @@ describe('Update User Use Case', () => {
 
         await expect(promise).rejects.toThrow();
     });
+
+    it('should throw if UpdateUserRepository throws', async () => {
+        const { sut, updateUserRepository } = makeSut();
+        jest.spyOn(updateUserRepository, 'execute').mockRejectedValue(
+            new Error(),
+        );
+
+        const promise = sut.execute(faker.string.uuid(), {
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email,
+            password: user.password,
+        });
+
+        await expect(promise).rejects.toThrow();
+    });
 });
