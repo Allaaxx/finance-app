4189f8d872408e867292386fdf281b6f21abb65b
Allaaxx <allanrao358@gmail.com>
2026-06-08 11:57:15 -0300

    test: ensure PostgresCreateUserRepository calls Prisma with correct params

--- src/repositories/postgres/user/create-user.test.js
+++ src/repositories/postgres/user/create-user.test.js
@@ -1,3 +1,4 @@
+import { prisma } from '../../../../prisma/prisma';
 import { user } from '../../../tests';
 import { PostgresCreateUserRepository } from './create-user';
 
@@ -13,5 +14,14 @@ describe('Create User Repository', () => {
         expect(result.password).toBe(user.password);
     });
 
+    it('should call Prisma with correct params', async () => {
+        const sut = new PostgresCreateUserRepository();
+        const prismaSpy = jest.spyOn(prisma.user, 'create');
 
+        await sut.execute(user);
+
+        expect(prismaSpy).toHaveBeenCalledWith({
+            data: user,
+        });
+    })
 });
