9b970a4c0b21097d89a38b1cd4656dcfb7291fce
Allaaxx <allanrao358@gmail.com>
2026-06-17 06:53:01 -0300

    test: ensure PostgresGetUserByIdRepository calls Prisma with correct params

--- src/repositories/postgres/user/get-user-by-id.test.js
+++ src/repositories/postgres/user/get-user-by-id.test.js
@@ -12,5 +12,19 @@ describe('Postgres Get User By Id Repository', () => {
 
         expect(result).toStrictEqual(user)
     })
+
+    it('should call Prisma with correct params', async () => {
+        const sut = new PostgresGetUserByIdRepository()
+
+        const prismaSpy = jest.spyOn(prisma.user, 'findUnique')
+
+        await sut.execute(fakeUser.id)
+
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                id: fakeUser.id
+            },
+        })
+    })
 })
 
