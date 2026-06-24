5760ddeb171540cb6313427091d9a9e2a1465622
Allaaxx <allanrao358@gmail.com>
2026-06-24 08:09:02 -0300

    refactor: move transaction routes to separete file

--- index.js
+++ index.js
@@ -1,55 +1,15 @@
 import 'dotenv/config.js';
 import express from 'express';
-import {
-    makeCreateTransactionController,
-    makeDeleteTransactionController,
-    makeGetTransactionsByUserIdController,
-    makeUpdateTransactionController,
-} from './src/factories/controllers/transaction.js';
-import { usersRouter } from './src/routes/users.js';
 
-const app = express();
+import { usersRouter, transactionsRouter } from './src/routes/index.js';
+
+export const app = express();
 
 app.use(express.json());
 
 app.use('/api/users', usersRouter)
 
-app.get('/api/transactions', async (request, response) => {
-    const getTransactionsByUserIdController =
-        makeGetTransactionsByUserIdController();
-
-    const { statusCode, body } =
-        await getTransactionsByUserIdController.execute(request);
-
-    response.status(statusCode).send(body);
-});
-
-app.post('/api/transactions', async (request, response) => {
-    const createTransactionController = makeCreateTransactionController();
-
-    const { statusCode, body } =
-        await createTransactionController.execute(request);
-
-    response.status(statusCode).send(body);
-});
-
-app.patch('/api/transactions/:transactionId', async (request, response) => {
-    const updateTransactionController = makeUpdateTransactionController();
-
-    const { statusCode, body } =
-        await updateTransactionController.execute(request);
-
-    response.status(statusCode).send(body);
-});
-
-app.delete('/api/transactions/:transactionId', async (request, response) => {
-    const deleteTransactionController = makeDeleteTransactionController();
-
-    const { statusCode, body } =
-        await deleteTransactionController.execute(request);
-
-    response.status(statusCode).send(body);
-});
+app.use('/api/transactions', transactionsRouter)
 
 app.listen(process.env.PORT, () => {
     const url = `http://localhost:${process.env.PORT}`;
