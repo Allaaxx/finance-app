76b24e8b14f2f4742426ccfe74f4b25467a736e8
Allaaxx <allanrao358@gmail.com>
2026-06-24 11:25:12 -0300

    test: ensure PATCH /api/transactions/:transactionId returns 404 when transaction is not found

--- src/routes/transactions.e2e.test.js
+++ src/routes/transactions.e2e.test.js
@@ -99,4 +99,15 @@ describe('Transaction Routes E2E Tests', () => {
         expect(response.status).toBe(200)
         expect(response.body.id).toEqual(createdTransaction.id)
     })
+
+    it('PATCH /api/transactions/:transactionId should return 404 when transaction is not found', async () => {
+        const response = await request(app)
+            .patch(`/api/transactions/${transaction.id}`)
+            .send({
+                amount: 100,
+                type: TransactionType.INVESTMENT,
+            })
+
+        expect(response.status).toBe(404)
+    })
 })
